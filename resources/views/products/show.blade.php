e121de553e65731b39cd551a53ab1c2dd49aa9aa
paikwiki <paikwiki@gmail.com>
2019-01-23 10:31:08 +0900

    Add review form and list

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,9 +8,8 @@
                 <div class="card-header">
                     <h1>Product name</h1>
                 </div>
-
                 <div class="card-body">
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col-md-6">
                             <img src="https://picsum.photos/240/160/?random" alt="image" style="max-width: 100%;">
                         </div>
@@ -22,40 +21,70 @@
                             </ul>
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <div class="card mb-5">
-                <div class="card-header">
-                    <h2>Reviews</h2>
-                </div>
-
-                <div class="card-body">
-                    <div class="row mt-3 mb-3">
-                        <div class="col-md-2">
-                            <img src="https://picsum.photos/240/160/?random" alt="image" style="max-width: 100%;">
-                        </div>
-                        <div class="col-md-10">
-                            <p class="uname"><span>Jane</span> <span>★★★★☆</span></p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dignissimos, non consectetur voluptates nostrum tenetur! Eius voluptas iste quae ab eaque blanditiis. Tenetur debitis possimus, quam tempore voluptate quisquam officia!</p>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-md-2">
-                            <img src="https://picsum.photos/240/156/?random" alt="image" style="max-width: 100%;">
-                        </div>
-                        <div class="col-md-10">
-                            <p class="uname"><span>Jane</span> <span>★★★★☆</span></p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dignissimos, non consectetur voluptates nostrum tenetur! Eius voluptas iste quae ab eaque blanditiis. Tenetur debitis possimus, quam tempore voluptate quisquam officia!</p>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-md-2">
-                            <img src="https://picsum.photos/240/162/?random" alt="image" style="max-width: 100%;">
-                        </div>
-                        <div class="col-md-10">
-                            <p class="uname"><span>Jane</span> <span>★★★★☆</span></p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dignissimos, non consectetur voluptates nostrum tenetur! Eius voluptas iste quae ab eaque blanditiis. Tenetur debitis possimus, quam tempore voluptate quisquam officia!</p>
+                    <div class="row">
+                        <div class="card m-2">
+                            <div class="card-header">
+                                <h2>Review</h2>
+                            </div>
+                            <div class="card-body">
+                                <div class="row mt-3 mb-3">
+                                    <form>
+                                        <h3>Write your review</h3>
+                                        <div class="form-group">
+                                            <label for="formTitleInput">Title</label>
+                                            <input type="text" class="form-control" id="formTitleInput" placeholder="Example input">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="formContentInput">Content</label>
+                                            <textarea class="form-control" id="formContentInput" rows="3"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                                    </form>
+                                </div>
+                                <div class="row mt-3 mb-3">
+                                    <h3>Reviews</h3>
+                                </div>
+                                <div class="row mt-3 mb-3">
+                                    <div class="col-md-2">
+                                        <img src="https://picsum.photos/240/162/?random" alt="image" style="max-width: 100%;">
+                                    </div>
+                                    <div class="col-md-10">
+                                        <div class="col-md-12">
+                                            <span class="uname">Jane</span>
+                                            <span class="stars">★★★★☆</span>
+                                            <div class="btn-group" role="group" aria-label="Basic example">
+                                                <button type="button" class="btn btn-secondary">Edit</button>
+                                                <button type="button" class="btn btn-danger">Delete</button>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dignissimos, non consectetur voluptates nostrum tenetur! Eius voluptas iste quae ab eaque blanditiis. Tenetur debitis possimus, quam tempore voluptate quisquam officia!</p>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-md-2">
+                                        <img src="https://picsum.photos/240/164/?random" alt="image" style="max-width: 100%;">
+                                    </div>
+                                    <div class="col-md-10">
+                                        <div class="col-md-12">
+                                            <span class="uname">Jane</span>
+                                            <span class="stars">★★★★☆</span>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dignissimos, non consectetur voluptates nostrum tenetur! Eius voluptas iste quae ab eaque blanditiis. Tenetur debitis possimus, quam tempore voluptate quisquam officia!</p>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col-md-2">
+                                        <img src="https://picsum.photos/240/158/?random" alt="image" style="max-width: 100%;">
+                                    </div>
+                                    <div class="col-md-10">
+                                        <div class="col-md-12">
+                                            <span class="uname">Jane</span>
+                                            <span class="stars">★★★★☆</span>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dignissimos, non consectetur voluptates nostrum tenetur! Eius voluptas iste quae ab eaque blanditiis. Tenetur debitis possimus, quam tempore voluptate quisquam officia!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
